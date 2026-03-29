fa6b83ca08b30639dc00d176339323d6db448ca2
T0501367-Dalaijargal <T0501367@my.ntu.ac.uk>
2026-03-29 18:56:43 +0100

    2.2

---
+++ 455.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE IF NOT EXISTS dvd_rental_db;
+USE dvd_rental_db;
+
+DROP TABLE IF EXISTS LOAN_COPY;
+DROP TABLE IF EXISTS LOAN;
+DROP TABLE IF EXISTS COPY;
+DROP TABLE IF EXISTS DVD;
+DROP TABLE IF EXISTS RENTAL_CATEGORY;
+DROP TABLE IF EXISTS BORROWER;
+
+CREATE TABLE BORROWER (
+    Borrower_No      VARCHAR(10)   NOT NULL,
+    Borrower_Name    VARCHAR(50)   NOT NULL,
+    Borrower_Address VARCHAR(100)  NOT NULL,
+    Borrower_Status  VARCHAR(20)   NOT NULL DEFAULT 'Allowed',
+    PRIMARY KEY (Borrower_No)
+);
+
+CREATE TABLE RENTAL_CATEGORY (
+    Rental_Category  VARCHAR(20)   NOT NULL,
+    Rental_Cost      DECIMAL(4,2)  NOT NULL,
+    PRIMARY KEY (Rental_Category)
+);
+
+CREATE TABLE DVD (
+    DVD_No             VARCHAR(10)   NOT NULL,
+    DVD_Title          VARCHAR(100)  NOT NULL,
+    DVD_Starring_Actor VARCHAR(50)   NOT NULL,
+    DVD_Year           INT           NOT NULL,
+    Rental_Category    VARCHAR(20)   NOT NULL,
+    PRIMARY KEY (DVD_No),
+    FOREIGN KEY (Rental_Category) REFERENCES RENTAL_CATEGORY(Rental_Category)
+);
+
+CREATE TABLE COPY (
+    Copy_No          VARCHAR(10)   NOT NULL,
+    DVD_No           VARCHAR(10)   NOT NULL,
+    Shelf_Position   VARCHAR(10)   NOT NULL,
+    PRIMARY KEY (Copy_No),
+    FOREIGN KEY (DVD_No) REFERENCES DVD(DVD_No)
+);
+
